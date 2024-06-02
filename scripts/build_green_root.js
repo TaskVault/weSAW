8f234d89e471783e1127d741c3e11dc412ea70a3
gmuratov <26muratov@gmail.com>
2024-06-02 10:03:58 +0200

    scripts

---
+++ scripts/build_green_root.js
@@ -0,0 +1,21 @@
+const { ShardedMerkleTree } = require("../src/merkle");
+const { tokens } = require("../data/green_tokens.json");
+
+(async () => {
+  let entries = [];
+  let shardNybbles = 1;
+  let target = `providers/bab`;
+
+  if (hre.network.name === "hardhat") {
+    const signers = await ethers.getSigners();
+    entries = signers.slice(0, 10).map((signer) => [signer.address, 1]);
+    target = `providers/test_bab`;
+  } else {
+    shardNybbles = 2;
+    entries = Object.entries(tokens).map(([key, value]) => [value, key]);
+  }
+  ShardedMerkleTree.build(entries, shardNybbles, target);
+
+  const tree = ShardedMerkleTree.fromFiles(target);
+  console.log("Merkle Root", tree.root);
+})();
